4c1724c3a322c8eb0c4096d65e8ac13fd816f67f
Levy Sampaio <levysampaio@live.com>
2021-10-01 11:31:36 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿namespace composicaoCSharp.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double Subtotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
